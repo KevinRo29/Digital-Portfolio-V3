5a6ca8ba55be092c495dcd5eb789232d5d193eac
Kevin Romero <jair.romero@orientapae.com>
2026-02-01 16:52:11 -0600

    Se suben urls correctas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', loadComponent: () => import('./features/home/home').then(m => m.Home) }
+    { path: 'home', loadComponent: () => import('./features/home/home').then(m => m.Home) },
+    { path: '**', redirectTo: 'home' },
 ];
